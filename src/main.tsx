7f3d325a77ff3e6f1bc95253edb8c54f96b796db
Alex Morariu <almor@Alexs-MacBook-Pro.local>
2023-04-06 15:05:16 -0400

    todo-complete

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+)
+
+
